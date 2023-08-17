70ba05ad33005de3ce6091bdda43cea48b72a673
dat <andyzdinh08@gmail.com>
2023-08-17 11:04:17 -0700

    fixed scss,renamed parent class in MainVideoDescri deleted css files and fixed path to scss

--- src/components/MainVideo/MainVideoDescription/MainVideoDescription.jsx
+++ src/components/MainVideo/MainVideoDescription/MainVideoDescription.jsx
@@ -3,30 +3,29 @@ import views from "../../../assets/Icons/views.svg";
 import likes from "../../../assets/Icons/likes.svg";
 
 function MainVideoDescription(props) {
-
     let d = new Date(props.currentVideo.timestamp);
     let date = `${(d.getMonth() + 1)}/${d.getDate()}/${d.getFullYear()}`;
 
     return (
-        <div className="video__info">
-            <h1 className="video__title">{props.currentVideo.title}</h1>
-            <div className="video__details">
-                <div className="video__left">
-                    <p className="video__author video__text">By {props.currentVideo.channel}</p>
-                    <p className="video__date video__text">{date}</p>
+        <div className="MainVideoDescription__info">
+            <h1 className="MainVideoDescription__title">{props.currentVideo.title}</h1>
+            <div className="MainVideoDescription__details">
+                <div className="MainVideoDescription__left">
+                    <p className="MainVideoDescription__author MainVideoDescription__text">By {props.currentVideo.channel}</p>
+                    <p className="MainVideoDescription__date MainVideoDescription__text">{date}</p>
                 </div>
-                <div className="video__right">
-                    <div className="video__views">
-                        <img src={views} alt="Views icon" className="video__icon"></img>
-                        <p className="video__views--text video__text">{props.currentVideo.views} </p>
+                <div className="MainVideoDescription__right">
+                    <div className="MainVideoDescription__views">
+                        <img src={views} alt="Views icon" className="MainVideoDescription__icon"></img>
+                        <p className="MainVideoDescription__views--text MainVideoDescription__text">{props.currentVideo.views}</p>
                     </div>
-                    <div className="video__likes">
-                        <img src={likes} alt="Likes icon" className="video__icon"></img>
-                        <p className="video__likes--text video__text">{props.currentVideo.likes} </p>
+                    <div className="MainVideoDescription__likes">
+                        <img src={likes} alt="Likes icon" className="MainVideoDescription__icon"></img>
+                        <p className="MainVideoDescription__likes--text MainVideoDescription__text">{props.currentVideo.likes}</p>
                     </div>
                 </div>
             </div>
-            <p className="video__description">{props.currentVideo.description} </p>
+            <p className="MainVideoDescription__paragraph">{props.currentVideo.description}</p>
         </div>
     );
 }
